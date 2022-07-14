50e8f015f2893444be063489dd2535eba2948488
刘刚 <2cevening@gmail.com>
2022-07-15 06:12:23 +0800

    feature(velocity-first-use):变量引用，属性引用，方法引用 🧵

---
+++ velocity-first-use/src/main/java/io/github/cocodx/first/entity/User.java
@@ -0,0 +1,15 @@
+package io.github.cocodx.first.entity;
+
+import lombok.Data;
+
+/**
+ * @author amazfit
+ * @date 2022-07-15 上午5:38
+ **/
+@Data
+public class User {
+
+    private String userName;
+    private String password;
+    private String email;
+}
--- velocity-first-use/src/test/java/io/github/cocodx/first/VelocityTest.java
+++ velocity-first-use/src/test/java/io/github/cocodx/first/VelocityTest.java
@@ -1,5 +1,6 @@
 package io.github.cocodx.first;
 
+import io.github.cocodx.first.entity.User;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.Velocity;
@@ -8,6 +9,7 @@ import org.junit.Test;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Date;
 import java.util.Properties;
 
 /**
@@ -33,14 +35,63 @@ public class VelocityTest {
         Velocity.init(properties);
 
         VelocityContext velocityContext = new VelocityContext();
-        velocityContext.put("name","刚子哥");
+        velocityContext.put("name1","刚子哥");
 
-        Template template = Velocity.getTemplate("vms/01-quickstart.vm", "utf-8");
+        Template template = Velocity.getTemplate("vms/02-cite-variable.vm", "utf-8");
 
-        FileWriter fileWriter = new FileWriter("D:\\code\\IDEA_workspace\\velocity-parent\\velocity-first-use\\src\\main\\resources\\html\\02-quickstart.html");
+        FileWriter fileWriter = new FileWriter("D:\\code\\IDEA_workspace\\velocity-parent\\velocity-first-use\\src\\main\\resources\\html\\03-quickstart.html");
 
         template.merge(velocityContext,fileWriter);
 
         fileWriter.close();
     }
+
+    @Test
+    public void test2() throws IOException {
+        //1. 设置velocity的资源加载器
+        Properties properties = new Properties();
+        properties.put("file.resource.loader.class","org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+
+        Velocity.init(properties);
+
+        VelocityContext velocityContext = new VelocityContext();
+        User user = new User();
+        user.setUserName("zhangsan");
+        user.setPassword("1q2w3e");
+        user.setEmail("860721890@qq.com");
+
+        velocityContext.put("user",user);
+
+        Template template = Velocity.getTemplate("vms/03-cite-field.vm", "utf-8");
+
+        FileWriter fileWriter = new FileWriter("D:\\code\\IDEA_workspace\\velocity-parent\\velocity-first-use\\src\\main\\resources\\html\\03-cite-field.html");
+
+        template.merge(velocityContext,fileWriter);
+
+        fileWriter.close();
+    }
+
+
+    @Test
+    public void test3() throws IOException {
+        //1. 设置velocity的资源加载器
+        Properties properties = new Properties();
+        properties.put("file.resource.loader.class","org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+
+        Velocity.init(properties);
+
+        VelocityContext velocityContext = new VelocityContext();
+
+        velocityContext.put("str","hello word velocity");
+        velocityContext.put("now",new Date());
+
+        Template template = Velocity.getTemplate("vms/04-cite-method.vm", "utf-8");
+
+        FileWriter fileWriter = new FileWriter("D:\\code\\IDEA_workspace\\velocity-parent\\velocity-first-use\\src\\main\\resources\\html\\04-cite-method.html");
+
+        template.merge(velocityContext,fileWriter);
+
+        fileWriter.close();
+    }
+
 }
